4f704b4dd68981e87c152b9645ed9300db9ee7b5
kewablay <kojokewajunior123@gmail.com>
2024-10-29 16:23:27 +0000

    reconfigured firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: 'AIzaSyDrLWGWbk4Bo7iqtzJnJoPR32VA1XWKntQ',
-      authDomain: 'litetradex-cd92b.firebaseapp.com',
-      projectId: 'litetradex-cd92b',
-      storageBucket: 'litetradex-cd92b.appspot.com',
-      messagingSenderId: '48378045923',
-      appId: '1:48378045923:web:ea96d6c8e787d28d332cd6',
-      measurementId: 'G-JKL83WH4HY',
+      apiKey: "AIzaSyBl0r2iudcKtlk9d4YJDo0SshFoP1jlT0s",
+      authDomain: "lite-tradex.firebaseapp.com",
+      projectId: "lite-tradex",
+      storageBucket: "lite-tradex.appspot.com",
+      messagingSenderId: "581178906710",
+      appId: "1:581178906710:web:7f6554ee3d45319aa6c3f7",
+      measurementId: "G-F5EHSGTF3R"
     },
   };
   
