aa8e4957e9c4dcb3fb28911cee5114c15bd414b4
kewablay <kojokewajunior123@gmail.com>
2024-10-29 17:19:42 +0000

    added initial add-funds page

---
+++ src/app/pages/user/add-funds/add-funds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-funds',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-funds.component.html',
+  styleUrl: './add-funds.component.sass'
+})
+export class AddFundsComponent {
+
+}
