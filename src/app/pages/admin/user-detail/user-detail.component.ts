8ae0692ad2a443cda087d02b5cbce6860155252d
kewablay <kojokewajunior123@gmail.com>
2024-10-30 21:59:55 +0000

    added user detail page

--- src/app/pages/admin/user-detail/user-detail.component.ts
+++ src/app/pages/admin/user-detail/user-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../../components/card/card.component";
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ButtonModule, RouterLink],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.sass'
 })
