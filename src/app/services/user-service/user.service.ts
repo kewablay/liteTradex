6296173322db57b1d653b337209841499db02c73
kewablay <kojokewajunior123@gmail.com>
2024-11-01 12:39:40 +0000

    added update balance

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Auth, user, User } from '@angular/fire/auth';
+import { Auth, deleteUser, user, User } from '@angular/fire/auth';
 import {
   collection,
+  collectionData,
   doc,
   DocumentData,
   Firestore,
   getDoc,
   getDocs,
   query,
+  updateDoc,
   where,
 } from '@angular/fire/firestore';
-import { BehaviorSubject, catchError, from, map, Observable, of } from 'rxjs';
+import { BehaviorSubject, catchError, from, map, Observable, of, switchMap } from 'rxjs';
 import { LocalStorageService } from '../local-storage-service/local-storage.service';
 
 @Injectable({
@@ -37,23 +39,61 @@ export class UserService {
     return this.localStorageService.getItem('user')?.uid;
   }
 
-  getUserById(uid: string | undefined): Observable<any | undefined> {
+  getUserById(uid: string | undefined): Observable<DocumentData | undefined> {
     const usersCollection = collection(this.firestore, 'users');
     const q = query(usersCollection, where('uid', '==', uid));
+  
+    // Use collectionData with the query to get real-time updates
+    return collectionData(q, { idField: 'id' }).pipe(
+      map((docs) => docs.length > 0 ? docs[0] : undefined), // pick the first document if found
+      catchError(() => of(undefined)) // handle errors and emit undefined
+    );
+  }
+
+
+  updateUserMainBalance(userId: string, balance: any): Observable<void> {
+    console.log("update data: ", balance, userId);
+    const usersCollection = collection(this.firestore, 'users');
+    const q = query(usersCollection, where('uid', '==', userId));
 
     return from(getDocs(q)).pipe(
-      map((snapshot) => {
-        // Check if there's any data in the snapshot
+      switchMap((snapshot) => {
         if (!snapshot.empty) {
-          return snapshot.docs[0].data(); // Return the first matching user's data
+          const userDoc = snapshot.docs[0];
+          const userDocRef = doc(this.firestore, 'users', userDoc.id);
+          return updateDoc(userDocRef, { 'balance.mainWallet': balance });
         } else {
           throw new Error('User not found');
         }
       }),
-      // Optional: handle any errors by returning an observable with undefined
-      catchError(() => of(undefined))
+      catchError((error) => {
+        console.error('Error updating main balance:', error);
+        throw error;
+      })
     );
   }
+
+  updateUserProfitBalance(userId: string, balance: any): Observable<void> {
+    const usersCollection = collection(this.firestore, 'users');
+    const q = query(usersCollection, where('uid', '==', userId));
+
+    return from(getDocs(q)).pipe(
+      switchMap((snapshot) => {
+        if (!snapshot.empty) {
+          const userDoc = snapshot.docs[0];
+          const userDocRef = doc(this.firestore, 'users', userDoc.id);
+          return updateDoc(userDocRef, { 'balance.profitWallet': balance });
+        } else {
+          throw new Error('User not found');
+        }
+      }),
+      catchError((error) => {
+        console.error('Error updating profit balance:', error);
+        throw error;
+      })
+    );
+  }
+
 
   getUsers(): Observable<DocumentData[]> {
     const usersCollection = collection(this.firestore, 'users');
