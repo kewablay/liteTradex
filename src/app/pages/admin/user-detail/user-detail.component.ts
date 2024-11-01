25bbb25c4fa6c2f97c406f1acd4b19595d9676e3
kewablay <kojokewajunior123@gmail.com>
2024-11-01 04:09:07 +0000

    added user detail integration

--- src/app/pages/admin/user-detail/user-detail.component.ts
+++ src/app/pages/admin/user-detail/user-detail.component.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../components/card/card.component';
 import { ButtonModule } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { DialogModule } from 'primeng/dialog';
-import { BalanceFormComponent } from "../../../components/balance-form/balance-form.component";
+import { BalanceFormComponent } from '../../../components/balance-form/balance-form.component';
+import { DocumentData } from '@angular/fire/firestore';
+import { Observable, switchMap } from 'rxjs';
+import { UserService } from '../../../services/user-service/user.service';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CardComponent, ButtonModule, RouterLink, DialogModule, BalanceFormComponent],
+  imports: [
+    CardComponent,
+    ButtonModule,
+    RouterLink,
+    DialogModule,
+    BalanceFormComponent,
+    AsyncPipe,
+    DatePipe
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.sass',
 })
 export class UserDetailComponent {
   visible: boolean = false;
   dialogType: string = '';
+  user$!: Observable<DocumentData>;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private userService: UserService) {
+    this.user$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const userId = params.get('id');
+        return this.userService.getUserById(userId as string);
+      })
+    );
+  }
 
   handleEditMainWallet(): void {}
 
@@ -30,4 +50,3 @@ export class UserDetailComponent {
     this.visible = false;
   }
 }
- 
