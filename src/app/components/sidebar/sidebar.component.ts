d56a1707027e53c7f678f11ca559a0ccd69e376c
kewablay <kojokewajunior123@gmail.com>
2024-11-01 03:22:44 +0000

    added user profile integration to sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -5,17 +5,22 @@ import { UserService } from '../../services/user-service/user.service';
 import { AuthService } from '../../services/auth-service/auth-service.service';
 import { Notyf } from 'notyf';
 import { NOTYF } from '../../utils/notyf.token';
+import { LocalStorageService } from '../../services/local-storage-service/local-storage.service';
+import { DocumentData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink, AvatarModule],
+  imports: [RouterLinkActive, RouterLink, AvatarModule, AsyncPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.sass',
 })
 export class SidebarComponent {
   @Output() onTabChange = new EventEmitter<Event>();
   isAdmin!: boolean;
+  currentUser$!: Observable<DocumentData>;
 
   handleTabChange(): void {
     this.onTabChange.emit();
@@ -61,7 +66,11 @@ export class SidebarComponent {
     private userService: UserService,
     private authService: AuthService,
     @Inject(NOTYF) private notyf: Notyf
-  ) {}
+  ) {
+    this.currentUser$ = this.userService.getUserById(
+      this.userService.getCurrentUserId()
+    );
+  }
 
   ngOnInit() {
     this.userService.getUserRole().subscribe((role) => {
