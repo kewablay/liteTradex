4a97b4751d0f264d01918030021e9c9dc0935fd6
kewablay <kojokewajunior123@gmail.com>
2024-11-01 03:22:25 +0000

    updated card component with balances

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Wallet } from '../../models/app.model';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
 standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, DecimalPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.sass',
 })
 export class CardComponent {
   @Input() isAdminCard: boolean = false;
-  @Input() balance!: string;
+  @Input() balance!: Wallet;
   @Input() cardTitle!: string;
 
   @Output() onEdit = new EventEmitter<void>();
