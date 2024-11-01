c06b8689de8d566bf902b63a3c0846fdd8a3d533
kewablay <kojokewajunior123@gmail.com>
2024-11-01 03:20:24 +0000

    updated get user by id to return an observable

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User } from '@angular/fire/auth';
+import { Auth, user, User } from '@angular/fire/auth';
 import {
   collection,
   doc,
@@ -9,7 +9,8 @@ import {
   query,
   where,
 } from '@angular/fire/firestore';
-import { BehaviorSubject, from, map, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, from, map, Observable, of } from 'rxjs';
+import { LocalStorageService } from '../local-storage-service/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +18,11 @@ import { BehaviorSubject, from, map, Observable } from 'rxjs';
 export class UserService {
   private userRoleSubject = new BehaviorSubject<string | null>(null);
 
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private auth: Auth,
+    private localStorageService: LocalStorageService
+  ) {}
 
   getUserRole(): Observable<string | null> {
     return this.userRoleSubject.asObservable();
@@ -27,15 +32,26 @@ export class UserService {
     this.userRoleSubject.next(role);
   }
 
-  async getUserById(uid: string | undefined) {
+  getCurrentUserId(): string | undefined {
+    return this.localStorageService.getItem('user')?.uid;
+  }
+
+  getUserById(uid: string | undefined): Observable<any | undefined> {
     const usersCollection = collection(this.firestore, 'users');
     const q = query(usersCollection, where('uid', '==', uid));
 
-    const snapshot = await getDocs(q);
-    if (!snapshot.empty) {
-      return snapshot.docs[0].data(); // return the first matching user's data
-    } else {
-      throw new Error('User not found');
-    }
+    return from(getDocs(q)).pipe(
+      map((snapshot) => {
+        // Check if there's any data in the snapshot
+        if (!snapshot.empty) {
+          return snapshot.docs[0].data(); // Return the first matching user's data
+        } else {
+          throw new Error('User not found');
+        }
+      }),
+      // Optional: handle any errors by returning an observable with undefined
+      catchError(() => of(undefined))
+    );
   }
+  
 }
